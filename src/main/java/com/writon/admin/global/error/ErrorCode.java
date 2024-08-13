0fb1110af21314f6cb433137605ade6676d0fb5d
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-08-13 15:05:20 +0900

    refactor: 로그아웃 유저에러 추가

--- src/main/java/com/writon/admin/global/error/ErrorCode.java
+++ src/main/java/com/writon/admin/global/error/ErrorCode.java
@@ -23,7 +23,10 @@ public enum ErrorCode {
   NOT_FOUND(HttpStatus.NOT_FOUND, "404", "사용자를 찾을 수 없습니다"), // 404 Not Found
   METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "405", "허용되지 않은 메소드입니다"), // 405 Method Not Allowed
   CONFLICT(HttpStatus.CONFLICT, "409", "이미 가입한 사용자입니다"), // 409 Conflict
-  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버에 오류가 발생하였습니다"); // 500 Internal Server Error
+  INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "서버에 오류가 발생하였습니다"), // 500 Internal Server Error
+
+  // auth
+  LOGOUT_USER(HttpStatus.NOT_FOUND, "410", "로그아웃한 유저입니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
