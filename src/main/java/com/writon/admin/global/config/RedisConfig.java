255f1462cdfd58d9ab1cf2b7928be1e185dc605d
seungzzok <123801984+seungzzok@users.noreply.github.com>
2024-12-10 16:57:14 +0900

    fix: build 에러 수정

--- src/main/java/com/writon/admin/global/config/RedisConfig.java
+++ src/main/java/com/writon/admin/global/config/RedisConfig.java
@@ -1,24 +1,17 @@
 package com.writon.admin.global.config;
 
-import com.writon.admin.domain.repository.token.RefreshTokenRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-@EnableRedisRepositories(basePackages = "com.writon.admin.domain.repository.token",
-    includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-        classes = {RefreshTokenRepository.class}))
 public class RedisConfig {
 
   @Value("${spring.data.redis.host}")
